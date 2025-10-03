3a43d364d8591eba571d7760a4518595d45fdefd
jawed <jawed@LAPTOP-BTOBCIRH>
2025-10-03 21:37:18 +0530

    Assignment 2 Finished

---
+++ Assignments/com/jawedafsar/assignment2/Main.java
@@ -0,0 +1,14 @@
+package jawedafsar.assignment2;
+
+public class Main {
+	public static void main(String[] args) {
+        // Create Student object (default constructor prints message)
+        Student s1 = new Student();
+
+        // Create Commission object and test methods
+        Commission emp = new Commission();
+        emp.acceptDetails();
+        emp.calculateCommission();
+    }
+
+}
---
+++ Assignments/com/jawedafsar/assignment2/Student.java
@@ -0,0 +1,9 @@
+package jawedafsar.assignment2;
+
+public class Student {
+	    public Student() {
+	        System.out.println("Student object is created");
+	    }
+
+
+}
